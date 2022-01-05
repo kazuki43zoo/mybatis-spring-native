0fb7aa47f34e6de41ba69ffa6caac5178b9e76f8
Kazuki Shimizu <kazuki43zoo@gmail.com>
2022-01-05 11:12:25 +0900

    Add GitHub Action

--- mybatis-spring-native-samples/mybatis-spring-native-sample-simple/src/main/java/org/mybatis/spring/nativex/sample/simple/MybatisSpringNativeSampleApplication.java
+++ mybatis-spring-native-samples/mybatis-spring-native-sample-simple/src/main/java/org/mybatis/spring/nativex/sample/simple/MybatisSpringNativeSampleApplication.java
@@ -15,7 +15,6 @@
  */
 package org.mybatis.spring.nativex.sample.simple;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
@@ -23,7 +22,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@MapperScan(basePackages = "com.example", sqlSessionTemplateRef = "sqlSessionTemplate")
 @SpringBootApplication
 public class MybatisSpringNativeSampleApplication {
 
